9e35e0c4f794aacd026bbbd62529edb16bd5359f
ElinaGubanova <elina292017@gmail.com>
2024-11-09 15:55:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+homeworks = 12
+hours = 1.5
+course = 'Python'
+one_task_time = (hours / homeworks)
+
+print(f"Курс: {course}, всего задач:{homeworks}, затрачено часов: {hours}, среднее время выполнения {one_task_time} часа.")
+print("Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.")
